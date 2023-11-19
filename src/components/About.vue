c91d41d012f8d56f8ae7ad4a8b858ae12b838eba
Yuchen Zhu <yczhu@bu.edu>
2023-11-19 14:07:50 -0500

    change

---
+++ src/components/About.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="about-page">
+    <section class="about-intro">
+      <h1>About Us</h1>
+      <p>Welcome to WanderVoice – your intelligent companion on every journey.</p>
+      <!-- Additional intro content here -->
+    </section>
+
+    <section class="why-wander-voice">
+      <h2>Why WanderVoice?</h2>
+      <p>Traditional voice assistants aren't enough anymore...</p>
+      <!-- More content -->
+    </section>
+
+    <section class="team-behind">
+      <h2>Meet The Team Behind WanderVoice</h2>
+      <p>Founded by a team of passionate innovators...</p>
+      <!-- Team details -->
+    </section>
+
+    <section class="embark-journey">
+      <h2>Embark on a Journey Like Never Before</h2>
+      <p>Let WanderVoice be the soundtrack to your adventures...</p>
+      <!-- More details -->
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+  // You can include methods, data, computed properties, etc., as needed.
+}
+</script>
+
+<style scoped>
+.about-page {
+  /* Style your page according to the design */
+}
+
+.about-intro, .why-wander-voice, .team-behind, .embark-journey {
+  /* Apply common styles to each section */
+}
+
+/* Add additional styles for each section as needed */
+</style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import App from './App.vue'
-import Function from './components/Function.vue';
+import App from './App.vue';
 import Login from './components/Login.vue';
 import SignUp from './components/SignUp.vue';
+import Support from './components/Support.vue';
+import About from './components/About.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: '/', component: Login },
-  { path: '/sign-up', component: SignUp },
-  { path: '/function', component: Function },
-  { path: '/app', component: App }
+  { path: '/signup', component: SignUp },
+  { path: '/support', component: Support },
+  { path: '/app', component: App },
+  { path: '/about', component: About }
 ];
 // eslint-disable-next-line
 const router = new VueRouter({
