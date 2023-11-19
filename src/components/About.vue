664f5a57ed6565475a518f70cb69dcc8679fe856
Yuchen Zhu <yczhu@bu.edu>
2023-11-19 18:30:33 -0500

    CHANGE

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,36 +3,51 @@
     <Navbar />
     <section class="about-intro">
       <h1>About Us</h1>
-      <p>Welcome to WanderVoice – your intelligent companion on every journey.</p>
-      <!-- Additional intro content here -->
+      <p>Welcome to WanderVoice – your intelligent companion on every journey.\n
+      In today's world, in-car voice assistants have become an integral part of our driving experience. 
+      While they successfully set navigational goals and follow basic commands, there's a vast expanse of road untraveled when it comes to providing personalized, 
+      real-time suggestions and travel insights. That's where WanderVoice steps in.
+      </p>
     </section>
 
     <section class="why-wander-voice">
       <h2>Why WanderVoice?</h2>
-      <p>Traditional voice assistants aren't enough anymore...</p>
-      <!-- More content -->
+      <p>Traditional voice assistants just aren't enough anymore. Whether you're a tourist exploring unfamiliar terrains or a local in search of new experiences, 
+      WanderVoice is designed to enrich your journey. We recognized the need for a dynamic assistant – one that goes beyond the basic functionalities and delves 
+      into the realm of real-time travel guidance.\n
+      
+      With WanderVoice, you're not just getting from point A to B. Our advanced AI system serves dual purposes: acting as a travel chatbot, answering any questions 
+      you might have about your trip, and offering spontaneous real-time suggestions, ensuring you get the most out of every drive.\n
+
+      </p>
     </section>
 
     <section class="team-behind">
       <h2>Meet The Team Behind WanderVoice</h2>
-      <p>Founded by a team of passionate innovators...</p>
-      <!-- Team details -->
+      <p>Founded by a team of passionate innovators - Yuchen Zhu (yz4396), Yizhan Zhang (yz4703), and Xinyang Wang (xw2964). 
+      WanderVoice is a testament to our collective vision. Our dedication stems from understanding drivers' needs and 
+      our aim to revolutionize the driving experience.</p>
+
+
     </section>
 
     <section class="embark-journey">
       <h2>Embark on a Journey Like Never Before</h2>
-      <p>Let WanderVoice be the soundtrack to your adventures...</p>
-      <!-- More details -->
+      <p>Let WanderVoice be the soundtrack to your adventures, offering insights, recommendations, and answering every curious thought that might cross your mind during a drive. 
+      It's not about the destination but the journey, and with WanderVoice, you're guaranteed a memorable one.
+      Join us and redefine your driving experience. Welcome to WanderVoice – Where every journey has a story.</p>
     </section>
   </div>
 </template>
 
 <script>
-import Navbar from './components/navbar.vue';
+import Navbar from './navbar.vue';
 
 export default {
   name: 'AboutPage',
-  // You can include methods, data, computed properties, etc., as needed.
+  components: {
+    Navbar
+  },
 }
 </script>
 
